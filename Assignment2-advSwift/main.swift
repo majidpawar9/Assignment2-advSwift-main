93dce52879656e51633d1aec866c6cda20e7f012
majidpawar9 <majidpawar9@gmail.com>
2023-06-18 17:00:29 -0400

    Tested Question 1

--- Assignment2-advSwift/main.swift
+++ Assignment2-advSwift/main.swift
@@ -7,5 +7,14 @@
 
 import Foundation
 
+// Question1 Test Case
 print("Hello, World!")
+print(calculateArea(height: 4, width: 4))
+print(allCap(string: "majid"))
+var arr1 = [1,2,3,4,5,6]
+var arr2 = [1,2,3,4,5]
+let a1 = middle(array: arr1)
+let a2 = middle(array: arr2)
+print(a1)
+print(a2!)
 
