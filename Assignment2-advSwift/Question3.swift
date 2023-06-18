317d7b28dfae2f8ec39b538083fca3c4e7d4d4bc
majidpawar9 <majidpawar9@gmail.com>
2023-06-18 19:00:58 -0400

    Solved Question 3

--- Assignment2-advSwift/Question3.swift
+++ Assignment2-advSwift/Question3.swift
@@ -38,3 +38,58 @@ import Foundation
  - override upgradeRam() => if the ram is not upgradable do nothing
  - closeScreen() => this will set the laptop state to 'Sleep'
  */
+class Computer {
+    var cpu: String
+    var gpu: String
+    var ram: Int
+    var state: String
+    var temp: Double
+    
+    init(cpu: String, gpu: String, ram: Int, temp: Double) {
+        self.cpu = cpu
+        self.gpu = gpu
+        self.ram = ram
+        self.state = "Off"
+        self.temp = temp
+    }
+    
+    func upgradeRam(newAmount: Int) {
+        ram = newAmount
+    }
+    
+    func turnOn() {
+        state = "On"
+    }
+    
+    func turnOff() {
+        state = "Off"
+    }
+    
+    func getTemp() -> Double {
+        return temp
+    }
+}
+
+class Laptop: Computer {
+    var isRamUpgradable: Bool
+    var screenSize: Double
+    var isTouchScreen: Bool
+    
+    init(cpu: String, gpu: String, ram: Int, state: String, temp: Double, isRamUpgradable: Bool, screenSize: Double, isTouchScreen: Bool) {
+        self.isRamUpgradable = isRamUpgradable
+        self.screenSize = screenSize
+        self.isTouchScreen = isTouchScreen
+        super.init(cpu: cpu, gpu: gpu, ram: ram, temp: temp)
+        self.state = state
+    }
+    
+    override func upgradeRam(newAmount: Int) {
+        if isRamUpgradable {
+            super.upgradeRam(newAmount: newAmount)
+        }
+    }
+    
+    func closeScreen() {
+        state = "Sleep"
+    }
+}
