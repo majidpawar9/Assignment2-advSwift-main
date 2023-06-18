01371544250ca0402caebcc3fa69e8f6731d6ae0
majidpawar9 <majidpawar9@gmail.com>
2023-06-18 19:19:48 -0400

    Tested Question 3 (Testing of all Questions done)

--- Assignment2-advSwift/main.swift
+++ Assignment2-advSwift/main.swift
@@ -21,3 +21,27 @@ print(a2!)
 // Question2 Test Case
 let rec = Rectangle(height: 2, width: 2, color: "Black")
 rec.printColor()
+
+// Question3 Test Case
+// Computer Class
+let comp = Computer(cpu: "8-core", gpu: "10-core", ram: 8, temp: 32)
+print(comp.state)
+comp.turnOn()
+print(comp.state)
+print(comp.ram)
+comp.upgradeRam(newAmount: 16)
+print(comp.ram)
+print(comp.getTemp())
+
+// Laptop Class
+let lap = Laptop(cpu: "16-core", gpu: "12-core", ram: 32, state: "On", temp: 42, isRamUpgradable: true, screenSize: 14, isTouchScreen: true)
+print(lap.state)
+lap.turnOn()
+print(lap.state)
+print(lap.ram)
+lap.upgradeRam(newAmount: 64)
+print(lap.ram)
+print(lap.getTemp())
+print(lap.state)
+lap.closeScreen()
+print(lap.state)
